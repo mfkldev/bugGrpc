8203e653d49801ad5ffb7649384707a538bc5059
mfkldev <marcio.souza@zup.com.br>
2021-08-16 22:09:54 -0300

    setup-initial

---
+++ src/test/kotlin/br/com/marciofranklin/BugGrpcTest.kt
@@ -0,0 +1,19 @@
+package br.com.marciofranklin
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import javax.inject.Inject
+
+@MicronautTest
+class BugGrpcTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
